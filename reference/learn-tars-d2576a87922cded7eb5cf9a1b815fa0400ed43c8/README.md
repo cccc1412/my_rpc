42bbb5aed56fc43672f31567b7aa74195943873c
JUSTME <674987879@qq.com>
2021-11-30 15:03:05 +0800

    multi thread

--- reference/learn-tars-d2576a87922cded7eb5cf9a1b815fa0400ed43c8/README.md
+++
@@ -1,2 +0,0 @@
-# learn-tars
-腾讯开源框架Tars源码学习笔记---从头搭建Tars rpc
--- reference/learn-tars-d2576a87922cded7eb5cf9a1b815fa0400ed43c8/run.sh
+++
@@ -1,2 +0,0 @@
-g++ -std=c++11 -o tar-demo tc_epoll_server.cpp main.cpp tc_epoller.cpp tc_socket.cpp tc_thread_cond.cpp tc_thread.cpp tc_ex.cpp tc_thread_mutex.cpp -lpthread
-g++ -std=c++11 -o client client.cpp -lpthread
